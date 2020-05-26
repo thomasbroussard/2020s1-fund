aa40e7e4f6eff9a2325bcc2306ac4ca262927d28
thomas <thomas.broussard@local.local>
2020-05-26 20:16:16 +0200

    * modifications

--- Geometry/src/fr/epita/geometry/launcher/Launcher.java
+++ Geometry/src/fr/epita/geometry/launcher/Launcher.java
@@ -6,16 +6,16 @@ import fr.epita.geometry.datamodel.Triangle;
 
 public class Launcher {
 	
-//	public static void main(String[] args) {
-//		Triangle t1 = new Triangle(3,3,2,5);
-//		System.out.println(t1.calculateArea());
-//		
-//		Circle c1 = new Circle(3);
-//		System.out.println(c1.calculatePerimeter());
-//		
-//		Square s1 = new Square(5);
-//		
-//		System.out.println(s1.calculateArea());
-//	}
+	public static void main(String[] args) {
+		Triangle t1 = new Triangle(3,3,2,5);
+		System.out.println(t1.calculateArea());
+		
+		Circle c1 = new Circle(3);
+		System.out.println(c1.calculatePerimeter());
+		
+		Square s1 = new Square(5);
+		
+		System.out.println(s1.calculateArea());
+	}
 	
 }
